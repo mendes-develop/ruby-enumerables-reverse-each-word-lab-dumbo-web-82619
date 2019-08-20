0ecc309b06afb70ad8c3d9aeb2717b3f78ab080b
Alex Mendes <mendes.develop@gmail.com>
2019-08-20 18:43:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   
   array = string.split(" ")
-  new_array = array.each {|n| n.reverse!}
+  new_array = array.collect {|n| n.reverse}
   new_array.map{ |i|"#{i}" }.join(' ')
   
 end
